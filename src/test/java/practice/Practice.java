fb9a7af368287f264cefb1b3e3c9da2c1c69bc62
patna <patnaik.ashish2703@gmail.com>
2023-03-28 11:31:02 +0530

    branch 1 first commit.

--- src/test/java/practice/Practice.java
+++ src/test/java/practice/Practice.java
@@ -11,5 +11,7 @@ public class Practice {
 		System.out.println("sample2");
 		System.out.println("hello");
 		System.out.println("This can be conflict");
+		System.out.println("hi");
+		System.out.println("branch");
 	}
 }
